32212f6522169df7b46597c10e31d4bd0ed79d2f
nathanziri <nathanziri@hotmail.com>
2020-11-03 10:21:39 -0500

    minor changes

--- src/main/resources/static/coreFunctions.js
+++ src/main/resources/static/coreFunctions.js
@@ -1,6 +1,6 @@
 function loadRoomLights()
 {
-    var loadedText = "";
+    var loadedText = '';
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -9,16 +9,16 @@ function loadRoomLights()
             {
                 for (var key2 of Object.keys(myObj[key1])){
                     if(key2=="name"){
-                        console.log(myObj[key1][key2][0]);
-                        //loadedText = loadedText.concat(myObj[key1][key2][0], " ");
+                        console.log(myObj[key1][key2][0].toString());
+                        loadedText += " " + myObj[key1][key2][0].toString();
                     }
                 }
             }
+            document.getElementById('SHCore').innerHTML = loadedText;
         };
     } 
     xmlhttp.open("GET", "layout.json", true);
     xmlhttp.send();
 
-    //document.getElementById('SHCore').innerHTML = "loadedText";
-    //console.log(loadedText);
+    console.log(loadedText);
 }
