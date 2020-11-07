ab7aad875bd35ca704b11d6e46c73651043ee2d1
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-11-07 18:00:43 -0500

    Refactor writing and loading from files

---
+++ src/main/java/com/soen343/backend/utilities/UserWriter.java
@@ -0,0 +1,49 @@
+package com.soen343.backend.utilities;
+
+import com.soen343.backend.factory.UserTypeFactory;
+import com.soen343.backend.model.User;
+
+import java.io.*;
+import java.util.List;
+import java.util.UUID;
+
+public class UserWriter {
+
+    private UserTypeFactory userFactory =  new UserTypeFactory();
+
+    public void saveUsers(List<User> userDB, String filepath)
+    {
+        try
+        {
+            FileOutputStream fos = new FileOutputStream(filepath,false);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+            System.out.print(userDB.size());
+            for (int i=0; i < userDB.size(); i++) {
+                User user = userDB.get(i);
+                bw.write(user.getRole());
+                bw.newLine();
+                bw.write(user.getId().toString());
+                bw.newLine();
+                bw.write(Boolean.toString(user.getUserPermissions().getCanLockDoors()));
+                bw.newLine();
+                bw.write(Boolean.toString(user.getUserPermissions().getCanOpenAllWindows()));
+                bw.newLine();
+                bw.write(Boolean.toString(user.getUserPermissions().getCanOpenRestrictedWindow()));
+                bw.newLine();
+                bw.write(Boolean.toString(user.getUserPermissions().getCanUseLights()));
+                bw.newLine();
+                bw.write(Boolean.toString(user.getUserPermissions().getCanUseRestrictedLights()));
+                bw.newLine();
+                bw.newLine();
+            }
+
+            bw.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace(System.out);
+        }
+    }
+
+
+}
