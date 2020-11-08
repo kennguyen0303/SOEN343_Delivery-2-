3b26745247f9ac58e4784929f5ba80289158ab2e
Shijun Deng <dengshijun233@gmail.com>
2020-11-07 19:58:34 -0500

    resolve conflict requirement

--- src/main/resources/static/SHP_functions.js
+++ src/main/resources/static/SHP_functions.js
@@ -148,7 +148,6 @@ function setAwayMode(){
             }
         }
     }
-<<<<<<< HEAD
 
     xhttp.open("GET", "http://localhost:8080/api/user/allUserRetrieval", true);
     xhttp.send();
@@ -171,6 +170,4 @@ function alertConsole(AlertType, timeOfAlert)
 //This method will write msg to a give log file
 function writeToFile(msg){
 
-=======
->>>>>>> 2dcc9510b0f671b202d7c47418ee3bd7279f02b9
 }
